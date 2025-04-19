5f1e62ecb22dcd89d539928e100898147297387e
TJMFL <161985881+TJMFL@users.noreply.github.com>
2025-04-19 00:52:08 -0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/auth/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-500 p-4">
       <LoginForm />
     </div>
   )
